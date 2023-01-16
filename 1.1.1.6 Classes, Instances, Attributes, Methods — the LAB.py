b6bc58d2267b318b40a9d63f87a862fcbe0cf395
ismooz <69241713+ismooz@users.noreply.github.com>
2023-01-16 09:42:21 +0100

    Add files via upload

---
+++ 1.1.1.6 Classes, Instances, Attributes, Methods — the LAB.py
@@ -0,0 +1,21 @@
+﻿class Mobile:
+    def __init__(self, number):
+        self.number = number
+        
+    def turn_on(self):
+        print(f"mobile phone {self.number} is turned on")
+        
+    def turn_off(self):
+        print("mobile phone is turned off")
+    
+    def call(self, number):
+        print(f"calling {number}")
+
+mobile_1 = Mobile('01632-960004')
+mobile_2 = Mobile('01632-960012')
+
+mobile_1.turn_on()
+mobile_2.turn_on()
+mobile_1.call('555-34343')
+mobile_1.turn_off()
+mobile_2.turn_off()
